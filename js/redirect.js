a22c1f87e77b168dd8c2af65d52dbdd34eed6bd7
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-05-08 18:53:26 -0500

    Agregar validación de códigos en el script de redirección para mejorar la gestión de entradas del usuario.

--- js/redirect.js
+++ js/redirect.js
@@ -10,15 +10,23 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
+    // Lista de códigos válidos
+    const validCodes = [
+        's18', 's17', 's16', 's15', 's14', 's13', 's12', 's11', 's10', 's9', 's8', 's7', 's6', 's5', 's4', 's3', 's2',
+        '210325', 'agos1', '140225', '030225', 'WHTRXNF', '230125', 'S925', 'XBB-7255'
+    ];
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         const codigo = input.value.trim();
 
         if (codigo) {
-            // Intentar redirigir a la página correspondiente
-            window.location.href = `html/${codigo}.html`;
-        } else {
-            alert('Por favor, ingrese un código.');
+            // Verificar si el código existe antes de redirigir
+            if (validCodes.includes(codigo)) {
+                window.location.href = `html/${codigo}.html`;
+            } else {
+                //alert('El código ingresado no existe. Por favor, verifique el código.');
+            }
         }
     });
 });
