9f8bdf5c87688ccb31212ec7389ed27ddb3a91f0
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-05-08 19:04:32 -0500

    Reorganizar scripts en index.html y mover la lista de códigos válidos a validCodes.js para mejorar la estructura del código y la legibilidad.

--- js/redirect.js
+++ js/redirect.js
@@ -10,12 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    // Lista de códigos válidos
-    const validCodes = [
-        's18', 's17', 's16', 's15', 's14', 's13', 's12', 's11', 's10', 's9', 's8', 's7', 's6', 's5', 's4', 's3', 's2',
-        '210325', 'agos1', '140225', '030225', 'WHTRXNF', '230125', 'S925', 'XBB-7255'
-    ];
-
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         const codigo = input.value.trim();
---
+++ js/validCodes.js
@@ -0,0 +1,8 @@
+// Lista de códigos válidos
+const validCodes = [
+    's18', 's17', 's16', 's15', 's14', 's13', 's12', 's11', 's10', 's9', 's8', 's7', 's6', 's5', 's4', 's3', 's2',
+    '210325', 'agos1', '140225', '030225', 'WHTRXNF', '230125', 'S925', 'XBB-7255'
+];
+
+// Exportar la variable para que pueda ser usada en otros archivos
+window.validCodes = validCodes; 
