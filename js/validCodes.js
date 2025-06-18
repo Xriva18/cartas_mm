cdba4057e0454a168609b5f7ea36c38249394c78
Xriva18 <kevinguanoluisa2004@gmail.com>
2025-06-17 22:23:46 -0500

    Agregar el código 's24' a la lista de códigos válidos en validCodes.js y crear el archivo 's24.html' con contenido de tarjeta.

--- js/validCodes.js
+++ js/validCodes.js
@@ -9,7 +9,8 @@ const validCodes = [
     's12', 's13', 's14',
     's15', 's16', 's17',
     's18', 's19', 's20',
-    's21', 's22', 's23'
+    's21', 's22', 's23',
+    's24'
 ];
 
 // Exportar la variable para que pueda ser usada en otros archivos
